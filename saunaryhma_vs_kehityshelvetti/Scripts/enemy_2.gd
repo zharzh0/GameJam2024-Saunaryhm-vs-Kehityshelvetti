f832d849d10c8d97420063ec8b045de878ee44fe
eahii <p86385@edu.sasky.fi>
2024-10-08 10:57:58 +0300

    eka enemy ja killzone joka ei toimi ":D"

---
+++ saunaryhma_vs_kehityshelvetti/Scripts/enemy_2.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+const SPEED = 60
+
+var direction = 1
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_2d_left: RayCast2D = $RayCast2DLeft
+#@onready var animated_sprite = $AnimatedSprite2D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+		#animated_sprite.flip_h = true
+	if ray_cast_2d_left.is_colliding():
+		direction = 1
+		#animated_sprite.flip_h = false
+		
+	position.x += direction * SPEED * delta
---
+++ saunaryhma_vs_kehityshelvetti/Scripts/killzone.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You died!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
